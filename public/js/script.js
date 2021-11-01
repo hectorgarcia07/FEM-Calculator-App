388130c81f8daab76069ed255462b25eb19ef4d5
Hector Garcia <hector.garcia07@utrgv.edu>
2021-11-01 15:10:11 -0500

    compleated the calculator function for the calculator

--- public/js/script.js
+++ public/js/script.js
@@ -1,62 +1,210 @@
-const eventContainer = document.getElementsByClassName('theme-switches')[0]
-const btnEventContainer = document.getElementsByClassName('btn-keys')[0]
-const inputScreen = document.getElementById('screen-input')
+//case when user enters decimal and they delete it
 
-const html = document.querySelector('html');
-html.dataset.theme = `theme-one`;
 
+const screenDisplay = document.getElementById('screen-input')
+const miniDisplay = document.getElementById('mini-display')
+const html = document.querySelector('html')
+const themeSwitcherBtn = document.querySelectorAll('input[name="theme"]')
+const digitKeys = document.querySelectorAll('[data-digit]')
+const decimalKey = document.querySelector('[data-decimal]')
+const deleteKey = document.querySelector('[data-delete]')
+const operationKeys = document.querySelectorAll('[data-operation]')
+const resetKey = document.querySelector('[data-reset]')
+const equalKey = document.querySelector('[data-equal]')
+
+//theme one is the default theme
+html.dataset.theme = `theme-one`
+
+//switches the theme based on given parameter
 function switchTheme(theme) {
-  html.dataset.theme = `theme-${theme}`;
+  html.dataset.theme = `theme-${theme}`
 }
 
-//when user clicks on radio button the event will bubble up and this
-//will capture it and change theme accordingly
-eventContainer.addEventListener('click', event => {
-  if(event.target.id == 'theme-one'){
-    switchTheme('one')
-  }
-  else if(event.target.id == 'theme-two'){
-    switchTheme('two')
-  }
-  else if(event.target.id == 'theme-three'){
-    switchTheme('three')
-  }
+//for each radio button switch the theme to its respective active state
+themeSwitcherBtn.forEach( radioBtn => {
+    radioBtn.addEventListener('click', event =>{
+        switchTheme(radioBtn.dataset.radio)
+    })
 })
 
-btnEventContainer.addEventListener('click', event => {
-  let key = event.target.dataset.key
-  console.log(key)
+//event for all key 0 - 9 click
+digitKeys.forEach( digitKey => {
+    digitKey.addEventListener('click', event => {
+        calculator.updateOperand(digitKey.dataset.digit)
+    })
+})
 
-  if(key){
-    switch(key){
-      case 'del':
-        deleteKey()
-        break
-      case key.match(/\d/).input:
-        appendNumber(key)
-        break
+//event for decimal click
+decimalKey.addEventListener('click', event => {
+    calculator.decimal()
+})
+
+//event for the delete key
+deleteKey.addEventListener('click', event => {
+    calculator.deleteDigit()
+})
+
+//event for each operation key
+operationKeys.forEach(operationKey => {
+    operationKey.addEventListener('click', event => {
+        calculator.operation(operationKey.dataset.operation)
+    })
+})
+
+//event to clear operands and operator
+resetKey.addEventListener('click', event => {
+    calculator.reset()
+})
+
+//calculate's the result
+equalKey.addEventListener('click', event => {
+    calculator.equal()
+})
+
+//calcuator object that will serve 
+class Calculator {
+    constructor(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operator = undefined
+        this.isPrevOperandSet = false
     }
-  }
-})
+    //will update the current operand
+    updateOperand(digit){
+        //if the first digit is zero, set it to the users number
+        //otherwise append to the end
+        if(this.currentOperand == '' || this.currentOperand == '0'){
+            this.currentOperand = digit
+        }
+        else{
+            this.currentOperand = this.currentOperand + digit
+        }
+        this.updateDisplay()
+    }
+    //will update the display to show current operand
+    updateDisplay(){
+        if(this.currentOperand == ''){
+            screenDisplay.textContent = '0'
+        }
+        else if(this.currentOperand[this.currentOperand.length - 1] == '.'){
+            screenDisplay.textContent = this.currentOperand
+        }else{
+            //splits the string for easy formating
+            const [value, decimalValue] = this.currentOperand.split('.')
+            const output = this.formatOutput(value, decimalValue)
 
-//removes last char in input
-function deleteKey(){
-  let value = inputScreen.value.slice(0, -1)
-  
-  if(value == ''){
-    value = 0
-  }
+            screenDisplay.textContent = output
+        }
+    }
 
-  inputScreen.value = value
+    //will return output as formated in en-IN
+    formatOutput(value, decimal){
+        let newVal = parseFloat(value).toLocaleString("en-IN", { useGrouping: true })
+        //if there are decmimal, append it
+        if(decimal){
+            return `${newVal}.${decimal}`
+        }
+        return `${newVal}`
+    }
+
+    //will append a decmial if one hasn't been added yet
+    decimal(){
+        if(!this.currentOperand.includes('.')){
+            this.currentOperand = this.currentOperand + '.'
+            this.updateDisplay()
+        }
+    }
+    //will delete digit
+    deleteDigit(){
+        if(this.currentOperand.length == 1){
+            this.currentOperand = ''
+        }
+        else if(this.currentOperand == 'Infinity'){
+            this.currentOperand = ''
+        }
+        else{
+            this.currentOperand = this.currentOperand.slice(0, -1)
+        }
+        this.updateDisplay()
+    }
+    //can compute the given operations or updates the opperator
+    operation(op){
+        //if the second operand exist, save it and set a new operand
+        if(!this.operator){
+            if(!this.currentOperand){
+                this.currentOperand = '0'
+            }
+            this.operator = op
+            this.previousOperand = this.currentOperand
+            this.currentOperand = ''
+            this.isPrevOperandSet = true
+
+            miniDisplay.textContent = `${this.previousOperand} ${this.operator}`
+        }
+        else if(this.isPrevOperandSet){
+            if(!this.currentOperand){
+                this.operator = op
+            }else{
+                //compute the current operand and previous operand
+                const result = this.compute()
+                this.operator = op
+                this.previousOperand = result.toString()
+                this.currentOperand = ''
+            }
+
+            miniDisplay.textContent = `${this.previousOperand} ${this.operator}`
+        }
+
+        this.updateDisplay()
+    }
+    //computes the result based on given operation
+    compute(){
+        if(!this.currentOperand){
+            this.currentOperand = '0'
+        }
+        switch(this.operator){
+            case '+':
+                return parseFloat(this.previousOperand) + parseFloat(this.currentOperand)
+                break;
+            case '-':
+                return parseFloat(this.previousOperand) - parseFloat(this.currentOperand)
+                break;
+            case '/':
+                return parseFloat(this.previousOperand) / parseFloat(this.currentOperand)
+                break;
+            case '*':
+                return parseFloat(this.previousOperand) * parseFloat(this.currentOperand)
+                break;
+        }
+    }
+
+    //resets the calculator
+    reset(){
+        this.currentOperand = '0'
+        this.previousOperand = '0'
+        this.operator = undefined
+        this.isPrevOperandSet = false
+        miniDisplay.textContent = '\u00a0'
+        this.updateDisplay()
+    }
+
+    //computes the result based on the two operands
+    equal(){
+        if(this.operator){
+            const result = this.compute()
+
+            console.log(result)
+
+            this.currentOperand = result.toString()
+
+            this.previousOperand = '0'
+            this.operator = undefined
+            this.isPrevOperandSet = false
+            miniDisplay.textContent = '\u00a0'
+
+            this.updateDisplay()
+        }
+    }
 }
 
-//appends number to the input
-function appendNumber(number){
-  if (inputScreen.value == '0'){
-    inputScreen.value = number
-  }
-  else{
-    inputScreen.value = inputScreen.value + number
-
-  }
-}
+const calculator = new Calculator()
